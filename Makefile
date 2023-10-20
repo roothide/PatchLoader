c38795d214062f6bffec932d9f6ed92d3898819c
RootHide <134120506+RootHide@users.noreply.github.com>
2023-10-20 15:43:53 +0800

    version 0.0.2

--- Makefile
+++ Makefile
@@ -8,10 +8,23 @@ include $(THEOS)/makefiles/common.mk
 LIBRARY_NAME = roothidepatch
 
 $(LIBRARY_NAME)_FILES = PatchLoader.c
-$(LIBRARY_NAME)_CFLAGS = -fobjc-arc
+# $(LIBRARY_NAME)_CFLAGS = -fobjc-arc
 $(LIBRARY_NAME)_INSTALL_PATH = /usr/lib
 
 include $(THEOS_MAKE_PATH)/library.mk
 
+MIRROR_PATH=/var/mobile/Library/pkgmirror
+
+before-package::
+	mkdir -p $(THEOS_STAGING_DIR)/var
+	ln -s / $(THEOS_STAGING_DIR)/var/jb
+
+	mkdir -p $(THEOS_STAGING_DIR)/$(MIRROR_PATH)/var
+	ln -s $(MIRROR_PATH) $(THEOS_STAGING_DIR)/$(MIRROR_PATH)/var/jb
+
+	mkdir -p $(THEOS_STAGING_DIR)/$(MIRROR_PATH)/usr/lib
+	ln -s $(MIRROR_PATH)/Library/MobileSubstrate/DynamicLibraries $(THEOS_STAGING_DIR)/$(MIRROR_PATH)/usr/lib/TweakInject
+
 clean::
 	rm -rf ./packages/*
+	
--- PatchLoader.c
+++ PatchLoader.c
@@ -8,61 +8,81 @@
 #include <mach-o/loader.h>
 #include <sys/syslimits.h>
 
-int64_t (*jbdswDebugMe)(void)=NULL;
+#include <sys/syslog.h>
+#define SYSLOG(...) {openlog("roothide",LOG_PID,LOG_AUTH);syslog(LOG_DEBUG, __VA_ARGS__);closelog();}
 
-typedef void (*InitPatches)(const char* path, const struct mach_header *header, intptr_t slide);
+int64_t (*jbdswDebugMe)(void) = NULL;
 
-static void image_load_handler(const char* path, const struct mach_header *header, intptr_t slide)
+typedef void (*InitPatches)(const char *path, const struct mach_header *header, intptr_t slide);
+
+static void image_load_handler(const char *path, const struct mach_header *header, intptr_t slide)
 {
-    char patcher[PATH_MAX]={0};
-    snprintf(patcher,sizeof(patcher),"%s.roothidepatch",path);
-    if(access(patcher, F_OK) == 0) 
+    char patcher[PATH_MAX] = {0};
+    snprintf(patcher, sizeof(patcher), "%s.roothidepatch", path);
+    if (access(patcher, F_OK) == 0)
     {
         assert(jbdswDebugMe() == 0);
 
-        void* handler = dlopen(patcher, RTLD_NOW);
+        void *handler = dlopen(patcher, RTLD_NOW);
         assert(handler != NULL);
-        
-        void* initpatches = dlsym(handler, "InitPatches");
+
+        void *initpatches = dlsym(handler, "InitPatches");
         assert(initpatches != NULL);
 
-        ((InitPatches)initpatches)(path,header,slide);
+        ((InitPatches)initpatches)(path, header, slide);
     }
 }
 
+bool load_init=false;
+
 static void image_load_callback(const struct mach_header *header, intptr_t slide)
 {
+    if(!load_init) return;
+
     int count = _dyld_image_count();
-    for(int i=0; i<count; i++) {
-        if(header == _dyld_get_image_header(i)) {
-            const char* path = _dyld_get_image_name(i);
-            //printf("image-load %p %p %s\n", header, (void*)slide, path);
-            if(!_dyld_shared_cache_contains_path(path))
+    for (int i = 0; i < count; i++)
+    {
+        if (header == _dyld_get_image_header(i))
+        {
+            const char *path = _dyld_get_image_name(i);
+            //SYSLOG("PatchLoader: image-load %p %p %s\n", header, (void *)slide, path);
+            if (!_dyld_shared_cache_contains_path(path))
                 image_load_handler(path, header, slide);
             break;
         }
     }
 }
 
-
 // int proc_regionfilename(int pid, void* address, void * buffer, uint32_t buffersize);
 
 // static void image_load_callback(const struct mach_header *header, intptr_t slide)
 // {
-//     printf("image-load %p %p\n", header, slide);
-//
 //     char path[PATH_MAX]={0};
 //     if(proc_regionfilename(getpid(), (void*)header, path, sizeof(path)) > 0)
 //     {
+//         SYSLOG("PatchLoader: image-load %p %p %s\n", header, (void *)slide, path);
 //         if(!_dyld_shared_cache_contains_path(path))
 //             image_load_handler(path, header, slide);
 //     }
-
 // }
 
 static void __attribute__((constructor)) initializer()
 {
-    *(void**)&jbdswDebugMe = dlsym(RTLD_DEFAULT, "jbdswDebugMe");
+    *(void **)&jbdswDebugMe = dlsym(RTLD_DEFAULT, "jbdswDebugMe");
 
     _dyld_register_func_for_add_image(image_load_callback);
+
+    // dlopen during register may break loaded vector (dyld didn't really lock the vector)
+
+    int count = _dyld_image_count();
+    for (int i = 0; i < count; i++)
+    {
+        void* header = (void*)_dyld_get_image_header(i);
+        uint64_t slide = _dyld_get_image_vmaddr_slide(i);
+        const char *path = _dyld_get_image_name(i);
+        if (!_dyld_shared_cache_contains_path(path))
+            image_load_handler(path, header, slide);
+    }
+
+    load_init=true;
 }
